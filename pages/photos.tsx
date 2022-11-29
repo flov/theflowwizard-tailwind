99071fa8f8ef7c18f4a50801347b97437ea83a08
Florian Vallen <florian.vallen@gmail.com>
2022-11-29 20:19:05 +0100

    PageSeo for photos page

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -5,6 +5,8 @@ import 'react-image-lightbox/style.css'
 import { images, CustomImage } from '@/data/images'
 import TwitterBanner from '@/components/TwitterBanner'
 import PhotoCredits from '@/components/PhotoCredits'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
 
 export default function Photos() {
   const [index, setIndex] = useState(-1)
@@ -21,46 +23,53 @@ export default function Photos() {
   const handleMoveNext = () => setIndex(nextIndex)
 
   return (
-    <div>
-      <header className="">
-        <div className="text-center space-y-1">
-          <div>
-            <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 leading-9 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-              Contact Juggling Photo Gallery
-            </h1>
+    <>
+      <PageSEO
+        title={siteMetadata.title + ' - Photos'}
+        description="Photo Gallery of the flow wizard"
+      />
+
+      <div>
+        <header className="">
+          <div className="text-center space-y-1">
+            <div>
+              <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 leading-9 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                Contact Juggling Photo Gallery
+              </h1>
+            </div>
           </div>
+        </header>
+
+        <TwitterBanner />
+
+        <div className="max-w-2xl mx-auto">
+          <p>
+            From the center of my ❤️, I thank all the talented photographers for capturing so many
+            moments from so many places that I have visited in my life. Most of these Photos have
+            been made between 2012 and 2022 at festivals and circus conventions around Australia and
+            Europe. I hope you enjoy them as much as I do.
+          </p>
+
+          <PhotoCredits />
         </div>
-      </header>
 
-      <TwitterBanner />
-
-      <div className="max-w-2xl mx-auto">
-        <p>
-          From the center of my ❤️, I thank all the talented photographers for capturing so many
-          moments from so many places that I have visited in my life. Most of these Photos have been
-          made between 2012 and 2022 at festivals and circus conventions around Australia and
-          Europe. I hope you enjoy them as much as I do.
-        </p>
-
-        <PhotoCredits />
+        <Gallery images={images} onClick={handleClick} enableImageSelection={false} />
+        {!!currentImage && (
+          /* @ts-ignore */
+          <Lightbox
+            mainSrc={currentImage.original}
+            imageTitle={currentImage.caption}
+            mainSrcThumbnail={currentImage.src}
+            nextSrc={nextImage.original}
+            nextSrcThumbnail={nextImage.src}
+            prevSrc={prevImage.original}
+            prevSrcThumbnail={prevImage.src}
+            onCloseRequest={handleClose}
+            onMovePrevRequest={handleMovePrev}
+            onMoveNextRequest={handleMoveNext}
+          />
+        )}
       </div>
-
-      <Gallery images={images} onClick={handleClick} enableImageSelection={false} />
-      {!!currentImage && (
-        /* @ts-ignore */
-        <Lightbox
-          mainSrc={currentImage.original}
-          imageTitle={currentImage.caption}
-          mainSrcThumbnail={currentImage.src}
-          nextSrc={nextImage.original}
-          nextSrcThumbnail={nextImage.src}
-          prevSrc={prevImage.original}
-          prevSrcThumbnail={prevImage.src}
-          onCloseRequest={handleClose}
-          onMovePrevRequest={handleMovePrev}
-          onMoveNextRequest={handleMoveNext}
-        />
-      )}
-    </div>
+    </>
   )
 }
