6b0a6f3720abb40b7cf199449cd5347cf55a9ecd
Florian Vallen <florian.vallen@gmail.com>
2022-11-30 01:26:36 +0100

    Unnecessary typescript error

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -17,19 +17,22 @@ const Dropdown: FC = () => {
       </Menu.Button>
       <Menu.Items className="absolute flex flex-col w-48 p-1 mt-2 text-white bg-gray-900 border border-gray-800 rounded">
         {links.map((link) => (
-          <Menu.Item className="flex items-center p-2 rounded hover:bg-gray-800" key={link.title}>
-            {({ active }) => (
-              <Link href={link.href}>
-                {link.title === 'Contact' ? (
-                  <GiGlassBall className="inline-block w-6 h-6 mr-2 text-blue-400" />
-                ) : (
-                  <SlFire className="inline-block w-6 h-6 mr-2 text-red-500" />
-                )}
+          <>
+            {/* @ts-ignore */}
+            <Menu.Item className="flex items-center p-2 rounded hover:bg-gray-800" key={link.title}>
+              {({ active }) => (
+                <Link href={link.href}>
+                  {link.title === 'Contact' ? (
+                    <GiGlassBall className="inline-block w-6 h-6 mr-2 text-blue-400" />
+                  ) : (
+                    <SlFire className="inline-block w-6 h-6 mr-2 text-red-500" />
+                  )}
 
-                {link.title}
-              </Link>
-            )}
-          </Menu.Item>
+                  {link.title}
+                </Link>
+              )}
+            </Menu.Item>
+          </>
         ))}
       </Menu.Items>
     </Menu>
